bc42ee389b12a9dd19d38662fff8a2c8fb8e5515
Jonathan <jonathansantos.vae@gmail.com>
2023-12-17 20:14:31 +0100

    he añadido air y una pagina de errores e imagen

--- web/main.go
+++ web/main.go
@@ -4,23 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Bienvenido a la pagina de inicio")
-}
-func productHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Aquí se muestran los productos")
-}
-func productDetailtHandler(w http.ResponseWriter, r *http.Request) {
-	productID := r.URL.Path[len("/product/detail/"):]
-	fmt.Fprintf(w, "Detalles del producto con ID: %s", productID)
-}
+
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/product", productHandler)
-	http.HandleFunc("/product/detail/", productDetailtHandler)
+	fs := http.FileServer(http.Dir("web/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		http.ServeFile(w, r, filepath.Join("web/templates/", "home.html"))
+	})
+
+	http.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request){
+		http.ServeFile(w, r, filepath.Join("web/templates/", "error.html"))
+	})
 
 	fmt.Println("Servidor está corriendo en http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
